240c7cba9b98837a65d39b6b308520b0fe8cbca1
omarclv <omarclaudev@gmail.com>
2022-10-27 20:19:48 -0300

    import btabs

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -36,7 +36,13 @@
                             </tbody>
                         </table>
 
-            
+            <div>
+  <b-tabs content-class="mt-3">
+    <b-tab title="First" active><p>I'm the first tab</p></b-tab>
+    <b-tab title="Second"><p>I'm the second tab</p></b-tab>
+    <b-tab title="Disabled" disabled><p>I'm a disabled tab!</p></b-tab>
+  </b-tabs>
+</div>
             
         </div>
         
@@ -46,6 +52,12 @@
 </template>
 
 <script>
+import {
+  
+  BTabs,
+  BTab
+} 
+from "bootstrap-vue";
 import axios from "axios";
 export default {
     name: 'table-evento-list-row',
