fceb6424e3e78fa4f361aa89bf9f8594770fdab1
omarclv <omarclaudev@gmail.com>
2022-11-05 02:43:45 -0300

    ruta

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('/postproducto/{cantidad}/{producto}', [App\Http\Controllers\inventar
 
 Route::get('/zonapistola', [App\Http\Controllers\HomeController::class, 'pistolear']);
 
-Route::get('/inventariofull', [App\Http\Controllers\HomeController::class, 'inventario']);
+Route::get('/inventariofull', [App\Http\Controllers\HomeController::class, 'inventariofull']);
 
 Route::get('/agregarproducto', [App\Http\Controllers\HomeController::class, 'producto']);
 
